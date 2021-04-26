1fcd1f5369c02b09176df495f83f4251b57b5071
Malik Ardhiansyah <ardhiansyahmalik1200@gmail.com>
2021-04-26 07:37:00 +0700

    Create first simple web service with NodeJS

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log('Initialize backend project');
+const http = require('http');
+
+/**
+ * Logika untuk menangani dan menanggapi request dituliskan pada fungsi ini
+ * 
+ * @param request: Objek yang berisikan informaso terkait permintaan
+ * @param response: Objek yang digunakan untuk menanggapi permintaan
+ */
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'text/html');
+    response.statusCode = 200;
+    response.end('<h1>Halo HTTP Server!</h1>');
+};
+
+const server = http.createServer(requestListener);
+
+const port = 2000;
+const host = 'localhost';
+
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`);
+})
